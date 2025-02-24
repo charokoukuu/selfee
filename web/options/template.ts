70a3f947bcbb7ac2837e3ebe34310484e7e38843
saito <razikon@gmail.com>
2025-02-24 22:51:47 +0900

    テンプレ減らす

--- web/options/template.ts
+++ web/options/template.ts
@@ -23,16 +23,7 @@ export const background: backgroundTypes[] = [
         prompt: "a man of img Portrait photo in front of a beautiful background, softly blurred, vibrant colors, natural lighting, head and shoulders in frame, serene and confident expression",
         image: "https://res.cloudinary.com/tabikobo/images/c_scale,w_448,h_299,dpr_2/f_auto,q_auto/v1689924072/wordpress_tabi-pocket/af8f10f4c90fd99737035b3a555994bb/af8f10f4c90fd99737035b3a555994bb.jpg?_i=AA"
     },
-    {
-        name: "Template 3",
-        prompt: "Prompt 3",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtFeJEYhNbcTrqSiQFp9rqJg59p1tSJmMV0ZS_hY5ofNFJqMY52ZghonOC-pvl7OZqjs8&usqp=CAU"
-    },
-    {
-        name: "Template 3",
-        prompt: "Prompt 3",
-        image: "https://beiz.jp/images_T/mountain/mountain_00009.jpg"
-    }
+
 ]
 
 // テンプレート画像
